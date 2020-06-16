85d1c33b6cc67863ed1d019ed9c3418bd462d14f
Nicolás Jarma <njarma@evoltis.com>
2020-06-16 09:30:38 -0300

    cambios consulta linq

--- bitsa-api-users/Bitsa.User.Api/Repositories/UsersRepository.cs
+++ bitsa-api-users/Bitsa.User.Api/Repositories/UsersRepository.cs
@@ -14,7 +14,33 @@ namespace Bitsa.User.Api.Repositories
         public UsersRepository(DomainContext context, IMapper mapper): base(context, mapper) {}
         public Task<UsersGetViewModel> GetById(int userId)
         {
-            var dbset = _context.users.Where(x => x.Id == userId).FirstOrDefault();
+
+            /*
+            Una alternatica utilizando expresiones lampda (método de extensión)
+
+            var dbset = _context.users.OrderByDescending(x => x.Entry_Date)
+                        .FirstOrDefault(x => x.Id == userId);
+            */
+
+            /* Empleando Sintáxis de consulta */
+            var dbset = (from u in _context.users
+                         where u.Id == userId
+                         orderby u.Entry_Date descending
+                         select new users
+                         {
+                             Id = u.Id,
+                             First_Name = u.First_Name,
+                             Last_Name = u.Last_Name,
+                             Alias = u.Alias,
+                             Password = u.Password,
+                             Email = u.Email,
+                             Entry_Date = u.Entry_Date,
+                             Enabled = u.Enabled,
+                             Balance = u.Balance,
+                             Administrator = u.Administrator
+                         })
+                         .FirstOrDefault();
+            
             return Task.FromResult(_mapper.Map<UsersGetViewModel>(dbset));
         }
     }
